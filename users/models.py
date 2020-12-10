7d1f5ba0210e7e7f02c713aab7a602ac26b4c6d3
Hoss <hossshakiba@oulook.com>
2020-12-10 20:05:59 +0330

    Made users's email unique and necessary

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+User._meta.get_field('email')._unique = True
+User._meta.get_field('email').blank = False
+User._meta.get_field('email').null = False
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
